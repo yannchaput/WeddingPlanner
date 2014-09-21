3a50eb57b379ca626dcdca18fca303fa003d1af6
yannchaput <yannchaput@gmail.com>
2014-09-21 20:33:27 +0200

    Customize pie chart

--- src/com/innovention/weddingplanner/BudgetPieFragment.java
+++ src/com/innovention/weddingplanner/BudgetPieFragment.java
@@ -8,6 +8,7 @@ import com.innovention.weddingplanner.utils.WeddingPlannerHelper;
 
 import android.annotation.SuppressLint;
 import android.app.Fragment;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -19,7 +20,9 @@ import android.widget.ImageButton;
 
 public class BudgetPieFragment extends Fragment {
 
-	private static final String CHART_URL = "http://chart.apis.google.com/chart?cht=p3&chs=400x200&chd=e:TNTNTNGa&chts=000000,16&chtt=A+Better+Web&chl=Hello|Hi|anas|Explorer&chco=FF5533,237745,9011D3,335423&chdl=Apple|Mozilla|Google|Microsoft";
+	// private static final String CHART_URL =
+	// "http://chart.apis.google.com/chart?cht=p3&chs=400x200&chd=e:TNTNTNGa&chts=000000,16&chtt=A+Better+Web&chl=Hello|Hi|anas|Explorer&chco=FF5533,237745,9011D3,335423&chdl=Apple|Mozilla|Google|Microsoft";
+	private static final String CHART_URL = "http://chart.apis.google.com/chart?cht=p3";
 
 	// Log
 	private static final String TAG = BudgetPieFragment.class.getSimpleName();
@@ -67,7 +70,34 @@ public class BudgetPieFragment extends Fragment {
 
 		viewPie = (WebView) rootView.findViewById(R.id.webViewPie);
 		viewPie.getSettings().setJavaScriptEnabled(true);
-		viewPie.loadUrl(CHART_URL);
+
+		// Build URL
+		float pie_width = viewPie.getResources().getDimension(
+				R.dimen.budget_pie_chart_width);
+		float pie_height = viewPie.getResources().getDimension(
+				R.dimen.budget_pie_chart_height);
+		String title = viewPie.getResources().getString(R.string.budget_pie_chart_title);
+
+		Uri.Builder builder = new Uri.Builder();
+		Uri chartUri = builder
+				.scheme("http")
+				.authority("chart.apis.google.com")
+				.appendPath("chart")
+				.appendQueryParameter("cht", "p3")
+				.appendQueryParameter(
+						"chs",
+						new StringBuilder().append(String.valueOf((int) pie_width))
+								.append("x").append(String.valueOf((int) pie_height))
+								.toString())
+				.appendQueryParameter("chd", "t:20,20,45,15") // Data
+				.appendQueryParameter("chts", "000000,16") // Title color and size
+				.appendQueryParameter("chtt", title)
+				.appendQueryParameter("chl", "Hello|Hi|anas|Explorer")
+				.appendQueryParameter("chf", "bg,t,00000000")
+				//.appendQueryParameter("chco", "FF5533,237745,9011D3,335423")
+				.appendQueryParameter("chdl", "Apple|Mozilla|Google|Microsoft")
+				.build();
+		viewPie.loadUrl(chartUri.toString());
 
 		// Load Ad
 		adView = new AdView(this.getActivity());
